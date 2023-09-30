4d454354622f9e8590e055713f307f840ba2b22a
cckk2913 <cckk2913@gmail.com>
2023-09-30 13:38:37 +0800

    edit group trans

--- src/components/Group.js
+++ src/components/Group.js
@@ -2,35 +2,30 @@ import React from "react";
 import { motion } from "framer-motion";
 import { fadeIn } from "../variants";
 
-
-const members = [
-  {
-    name: "陳弱水",
-    description:
-      "一些工作職務",
-    contactinfo: "連絡信箱或電話",
-  },
-  {
-    name: "一二三",
-    description:
-      "一些工作職務",
-      contactinfo: "連絡信箱或電話",
-  },
-  {
-    name: "四五六",
-    description:
-      "一些工作職務",
-      contactinfo: "連絡信箱或電話",
-  },
-  {
-    name: "七八九",
-    description:
-      "一些工作職務",
-      contactinfo: "連絡信箱或電話",
-  },
-];
-
 const Group = ({ t }) => {
+  const members = [
+    {
+      name: t("group_member_a_name"),
+      description: t("group_member_a_description"),
+      contactinfo: "連絡信箱或電話",
+    },
+    {
+      name: t("group_member_b_name"),
+      description: t("group_member_b_description"),
+      contactinfo: "連絡信箱或電話",
+    },
+    {
+      name: t("group_member_c_name"),
+      description: t("group_member_c_description"),
+      contactinfo: "連絡信箱或電話",
+    },
+    {
+      name: t("group_member_d_name"),
+      description: t("group_member_d_description"),
+      contactinfo: "連絡信箱或電話",
+    },
+  ];
+
   return (
     <section id="group" className="section ">
       <div className="container mx-auto">
@@ -45,14 +40,15 @@ const Group = ({ t }) => {
             className="flex-1"
           >
             <h2 className="text-section-title">組織架構</h2>
-            <p className=" mb-4">本中心之諮詢委員會，委員 7-11 人，由本校與中研院共同推薦關注研究漢學及相關人文學領域，辦理余先生學術思想相關活動
-            </p>          
+            <p className=" mb-4">
+              本中心之諮詢委員會，委員 7-11
+              人，由本校與中研院共同推薦關注研究漢學及相關人文學領域，辦理余先生學術思想相關活動
+            </p>
           </motion.div>
         </div>
-        
-       {/* memberblock row */}
+
+        {/* memberblock row */}
         <div className="flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 ">
-          
           <motion.div
             variants={fadeIn("left", 0.2)}
             initial="hidden"
@@ -71,22 +67,14 @@ const Group = ({ t }) => {
                     key={index}
                   >
                     <div className="max-w-[560px] pb-10 memberBlockItem">
-                      <h4 className="memberName">
-                        {name}
-                      </h4>
-                      <p className="memberDescription">
-                        {description}
-                      </p>
-                      <p className="memberContactInfo">
-                        {contactinfo}
-                      </p>
+                      <h4 className="memberName">{name}</h4>
+                      <p className="memberDescription">{description}</p>
+                      <p className="memberContactInfo">{contactinfo}</p>
                     </div>
-                    
                   </div>
                 );
-              }
-            )}   
-            </div>     
+              })}
+            </div>
           </motion.div>
         </div>
       </div>
