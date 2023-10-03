42aa305aa2bcd0f2272dbfad75173ba7acd83aac
cckk2913 <cckk2913@gmail.com>
2023-10-03 10:32:02 +0800

    debug

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -27,24 +27,24 @@ const Contact = ({ t }) => {
     onSubmit: async (values) => {
       console.log(values);
 
-      // Initialize the Google Spreadsheet instance
+      // 初始化 GoogleSpreadsheet instance
       const doc = new GoogleSpreadsheet(
         process.env.REACT_APP_GOOGLE_SHEETS_DOCUMENT_ID
       );
 
-      // Authenticate using the credentials from your JSON file
+      // 授權 using credentials from .env
       await doc.useServiceAccountAuth({
         client_email: process.env.REACT_APP_CLIENT_EMAIL,
         private_key: process.env.REACT_APP_PRIVATE_KEY.replace(/\\n/g, "\n"), // Handle line breaks
       });
 
-      // Load the Google Spreadsheet
+      // 讀取 Google Spreadsheet
       await doc.loadInfo();
 
-      // Select the sheet you want to write to (by id)
+      // 選擇要寫入的工作表 Sheet (by id)
       const sheet = doc.sheetsById[process.env.REACT_APP_SHEET_ID];
 
-      // Create a new row with the form data
+      // 用收到的 form data 創建新的 row
       await sheet.addRow({
         Name: values.name,
         Email: values.email,
